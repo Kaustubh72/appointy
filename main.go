69bb032c7147be4d9796bb39a291f717da695912
Kaustubh Singh <singhkaustubh98@gmail.com>
2020-10-19 14:19:32 +0530

    All end points covered

--- main.go
+++ main.go
@@ -1,57 +1,88 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"strings"
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
+// GLOBAL VARIABLE
 var Meetings []Meeting
 
-// STRUCTURE FOR DEFINING MEETING AS GIVEN IN THE PDF
+// STRUCTURE FOR DEFINING MEETING AS GIVEN IN PDF
 type Meeting struct {
-	Id                 string    `json:"Id"`
-	Title              string    `json:"Title"`
-	Participants       string    `json:"Participants"`
-	Start_Time         time.Time `json:"Start_Time"`
-	End_Time           time.Time `json:"End_Time"`
-	Creation_Timestamp time.Time `json:"Creation_Timestamp"`
+	// Id                 string `json:"Id "`
+	IdMeeting          string `json:"IdMeeting"`
+	Title              string `json:"Title"`
+	Participants       string `json:"Participants"`
+	Start_Time         string `json:"Start_Time"`
+	End_Time           string `json:"End_Time"`
+	Creation_Timestamp string `json:"Creation_Timestamp"`
 }
 
-// STRUCTURE FOR DEFINING PARTICIPANT AS GIVEN IN THE PDF
+// STRUCTURE FOR DEFINING PARTICIPANT AS GIVEN IN PDF
 type Participant struct {
 	Name  string `json:"Name"`
 	Email string `json:"Email"`
 	RSVP  string `json:"RSVP"`
 }
 
-// END POINT FOR "GET A MEETING USING ID"
+// END POINT FOR "GET A MEETING USING ID" AS GIVEN IN PDF : : COVERING END POINTS
 func returnMeetingOfId(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Println("Endpoint For: Listing Meeting of id X")
 	id := strings.SplitN(r.URL.Path, "/", 3)[2]
+	// fmt.Println(id[2])
 
 	for i := 0; i < len(Meetings); i++ {
-		if id == Meetings[i].Id {
+		if id == Meetings[i].IdMeeting {
+
+			// fmt.Fprintf(w, Meetings[i].Title)
 			json.NewEncoder(w).Encode(Meetings[i])
 		}
 	}
 }
 
 func MeetingOperations(w http.ResponseWriter, r *http.Request) {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://kshitij:qwerty123@cluster0.kfjyr.mongodb.net/appointly?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
+	fmt.Println("CONNECTION TO MONGO DB IS SUCCESSFULL")
+
 	var presented_query = r.URL.Query()
 	if len(presented_query["participant"]) != 0 {
 		w.Header().Set("Content-Type", "application/json")
-
 		for i := 0; i < len(Meetings); i++ {
 			fmt.Println(Meetings[i].Participants)
 			fmt.Println(presented_query["participant"][0])
 			if presented_query["participant"][0] == "\""+Meetings[i].Participants+"\"" {
-				fmt.Println("sad")
+				// fmt.Println("sad")
 				json.NewEncoder(w).Encode(Meetings[i])
 			}
 		}
@@ -59,13 +90,13 @@ func MeetingOperations(w http.ResponseWriter, r *http.Request) {
 	}
 	if len(presented_query["start"]) != 0 && len(presented_query["end"]) != 0 {
 		for i := 0; i < len(Meetings); i++ {
-			start_meeting_time, err := time.Parse(time.ANSIC, presented_query["start"][0])
-			end_meeting_time, err := time.Parse(time.ANSIC, presented_query["end"][0])
-			if err != nil {
-				fmt.Println(err)
+			fmt.Println(presented_query["start"][0])
+			fmt.Println(presented_query["end"][0])
+			fmt.Println(Meetings)
+			if presented_query["start"][0] == "\""+Meetings[i].Start_Time+"\"" && presented_query["end"][0] == "\""+Meetings[i].End_Time+"\"" {
+				json.NewEncoder(w).Encode(Meetings[i])
 			}
-			fmt.Println(start_meeting_time)
-			fmt.Println(end_meeting_time)
+
 		}
 		fmt.Println("ROUTE FOR RANGE MEETING CORRECTLY DONE")
 	}
@@ -77,9 +108,24 @@ func MeetingOperations(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			panic(err)
 		}
-	
+		log.Println(inMeet)
+		appointlyDB := client.Database("appointlyDB")
+		meetingsCollection := appointlyDB.Collection("meetings")
+		meetingResult, err := meetingsCollection.InsertOne(ctx, bson.D{
+			{Key: "Id", Value: inMeet.IdMeeting},
+			{Key: "Title", Value: inMeet.Title},
+			{Key: "Participants", Value: inMeet.Participants},
+			{Key: "Start_Time", Value: inMeet.Start_Time},
+			{Key: "End_Time", Value: inMeet.End_Time},
+			{Key: "Creation_Timestamp", Value: inMeet.Creation_Timestamp}})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(meetingResult.InsertedID)
 		just_now := time.Now()
-		var newMeeting Meeting = Meeting{Id: inMeet.Id, Title: inMeet.Title, Participants: inMeet.Participants, Start_Time: inMeet.Start_Time, End_Time: inMeet.End_Time, Creation_Timestamp: just_now}
+		var newMeeting Meeting = Meeting{IdMeeting: inMeet.IdMeeting, Title: inMeet.Title, Participants: inMeet.Participants, Start_Time: inMeet.Start_Time, End_Time: inMeet.End_Time, Creation_Timestamp: just_now.String()}
 		Meetings = append(Meetings, newMeeting)
 		json.NewEncoder(w).Encode(inMeet)
 		fmt.Println("SCHEDULE MEETING ROUTE")
@@ -90,15 +136,48 @@ func MeetingOperations(w http.ResponseWriter, r *http.Request) {
 func handleRequests() {
 	http.HandleFunc("/meeting/", returnMeetingOfId)
 	http.HandleFunc("/meetings", MeetingOperations)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 func main() {
 
-	Meetings = []Meeting{
-		Meeting{Id: "1", Title: "Appointy General meeting", Participants: "general@appointy.com", Start_Time: time.Now().UTC(), End_Time: time.Now().UTC(), Creation_Timestamp: time.Now().UTC()},
-		Meeting{Id: "2", Title: "Appointy Executive Meeting", Participants: "executive@appointy.com", Start_Time: time.Now().UTC(), End_Time: time.Now().UTC(), Creation_Timestamp: time.Now().UTC()},
-		Meeting{Id: "3", Title: "Appointy and Kaustubh's Meeting", Participants: "kaustubh@appointy.com", Start_Time: time.Now().UTC(), End_Time: time.Now().UTC(), Creation_Timestamp: time.Now().UTC()},
+	Meetings = []Meeting{}
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://kaustubh:abc123def@cluster0.kfjyr.mongodb.net/appointly?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
 	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
+	fmt.Println("CONNECTION TO MONGODB IS SUCCESSFULL")
+
+	appointlyDB := client.Database("appointlyDB")
+	meetingsCollection := appointlyDB.Collection("meetings")
+
+	cursor, err := meetingsCollection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var allMeetingsDB []bson.M
+	if err = cursor.All(ctx, &allMeetingsDB); err != nil {
+		log.Fatal(err)
+	}
+	for i := 0; i < len(allMeetingsDB); i++ {
+		var newMeeting Meeting = Meeting{IdMeeting: fmt.Sprint(allMeetingsDB[i]["Id"]), Title: fmt.Sprint(allMeetingsDB[i]["Title"]), Participants: fmt.Sprint(allMeetingsDB[i]["Participants"]), Start_Time: fmt.Sprint(allMeetingsDB[i]["Start_Time"]), End_Time: fmt.Sprint(allMeetingsDB[i]["End_Time"]), Creation_Timestamp: fmt.Sprint(allMeetingsDB[i]["Creation_Timestamp"])}
+		Meetings = append(Meetings, newMeeting)
+	}
+
 	handleRequests()
 }
